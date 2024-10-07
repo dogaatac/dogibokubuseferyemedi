fc80b2aaf5b7d06173c507a054c49ed6f7a164a6
dogaatac <dogaatac1905@gmail.com>
2024-10-07 18:44:08 +0300

    Update kur.sh

--- kur.sh
+++ kur.sh
@@ -17,3 +17,6 @@ cd
 git clone https://github.com/dogaatac/dogibokubuseferyemedi.git
 screen -dmS kole python3 calis.py 213.199.52.11 /root/tig-monorepo/target/release/tig-worker --download wasms --port 5115 --verbose
 screen -dmS online python3 online.py
+
+
+
