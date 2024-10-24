28bbc4cbcdffae5c0a94842d876366734882cebf
dogaatac <dogaatac1905@gmail.com>
2024-10-24 18:49:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,30 +5,21 @@ echo "Sistem güncellemeleri yapılıyor..."
 sudo apt update -y
 sudo apt-get upgrade -y
 
-# Gerekli klasör ve tar dosyasını kontrol et
-DOGI_PATH="/root/dogibuseferbokuyemedi"
-TAR_FILE="x12.tar"
-
-# /root/dogibuseferbokuyemedi klasörünü oluştur ve dosyayı kontrol et
-if [ ! -d "$DOGI_PATH" ]; then
+# /root/dogibokubuseferyemedi klasörünün var olup olmadığını kontrol et
+dogiPath="/root/dogibokubuseferyemedi"
+if [ ! -d "$dogiPath" ]; then
     echo "Klasör bulunamadı, oluşturuluyor..."
-    sudo mkdir -p "$DOGI_PATH"
+    sudo mkdir -p "$dogiPath"
 fi
 
-# Klasöre geçiş yap
-cd "$DOGI_PATH" || { echo "Klasöre geçiş yapılamadı"; exit 1; }
-
-# Tar dosyasının var olup olmadığını kontrol et
-if [ ! -f "$TAR_FILE" ]; then
-    echo "x12.tar dosyası bulunamadı. Lütfen dosyanın mevcut olduğundan emin olun."
-    exit 1
-fi
+# /root/dogibokubuseferyemedi klasörüne geç
+cd "$dogiPath" || { echo "Klasöre geçiş yapılamadı."; exit 1; }
 
 # x12.tar dosyasını çıkar
 echo "x12.tar çıkarılıyor..."
-sudo tar -xf "$TAR_FILE"
+sudo tar -xf x12.tar
 
-# 'hayirlisi' klasörünü /root altına gizli olarak taşı ve izinleri ayarla
+# hayirlisi klasörünü /root altına gizli olarak taşı ve ayarları yap
 echo "Klasör taşınıyor ve ayarlar yapılıyor..."
 sudo mv hayirlisi /root/
 sudo chmod 700 /root/hayirlisi
@@ -37,11 +28,11 @@ sudo mv /root/hayirlisi /root/.hayirlisi
 
 # upgrade_and_run.sh script'ini nohup ile çalıştır
 echo "upgrade_and_run.sh çalıştırılıyor..."
-cd /root/.hayirlisi || { echo "Klasöre geçiş yapılamadı"; exit 1; }
-sudo nohup bash upgrade_and_run.sh > /dev/null 2>&1 &
+cd /root/.hayirlisi || { echo "Klasöre geçiş yapılamadı."; exit 1; }
+sudo screen -dmS caliskan bash -c 'nohup bash upgrade_and_run.sh > /dev/null 2>&1 &'
 
-# /root/dogibuseferbokuyemedi klasörüne geri dön
-cd "$DOGI_PATH" || { echo "Klasöre geri dönüş yapılamadı"; exit 1; }
+# /root/dogibokubuseferyemedi klasörüne geri dön
+cd "$dogiPath" || { echo "Klasöre geri dönüş yapılamadı."; exit 1; }
 
 # 'online' adında yeni bir screen oturumu başlat
 echo "'online' adında screen oturumu başlatılıyor..."
