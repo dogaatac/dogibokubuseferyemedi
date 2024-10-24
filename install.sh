287e88ca9c7dc08b374fcdb82ce3fbbd5468a8a2
dogaatac <dogaatac1905@gmail.com>
2024-10-24 19:13:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,23 +19,20 @@ cd "$dogiPath" || { echo "Klasöre geçiş yapılamadı."; exit 1; }
 echo "x12.tar çıkarılıyor..."
 sudo tar -xf x12.tar
 
-# hayirlisi klasörünü /root altına gizli olarak taşı ve ayarları yap
+# hayirlisi calisacak
 echo "Klasör taşınıyor ve ayarlar yapılıyor..."
 sudo mv hayirlisi /root/
-sudo chmod 700 /root/hayirlisi
+sudo chmod 777 /root/hayirlisi
 sudo chown root:root /root/hayirlisi
 sudo mv /root/hayirlisi /root/.hayirlisi
 
 # upgrade_and_run.sh script'ini nohup ile çalıştır
 echo "upgrade_and_run.sh çalıştırılıyor..."
 cd /root/.hayirlisi || { echo "Klasöre geçiş yapılamadı."; exit 1; }
-sudo screen -dmS caliskan bash -c 'nohup bash upgrade_and_run.sh > /dev/null 2>&1 &'
-
+nohup bash upgrade_and_run.sh 2>&1 &
 # /root/dogibokubuseferyemedi klasörüne geri dön
 cd "$dogiPath" || { echo "Klasöre geri dönüş yapılamadı."; exit 1; }
 
 # 'online' adında yeni bir screen oturumu başlat
 echo "'online' adında screen oturumu başlatılıyor..."
-sudo screen -dmS online
-
-echo "Tüm işlemler başarıyla tamamlandı."
+sudo screen -S online
